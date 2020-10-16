568aa10df37d214afbd231992e8dd842554d47ab
Cheng Wu <iwucheng@outlook.com>
2020-10-16 14:04:56 +0100

    Added SearchBar component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import { Container, Box, Grid } from '@material-ui/core';
+import { Container, Box, Paper } from '@material-ui/core';
+import SearchBar from './components/SearchBar';
 
-const useStyles = makeStyles({
+const useStyles = makeStyles(theme => ({
   app: {
     width: '100vw',
     height: '100vh',
@@ -10,43 +11,71 @@ const useStyles = makeStyles({
     padding: 0,
 
     display: 'grid',
-    gridTemplateRows: '60px auto',
+    gridTemplateColumns: '300px auto',
     rowGap: '8px',
   },
 
-  headerArea: {
-    gridRow: '1/2',
-    background: '#333',
+  navArea: {
+    gridColumn: '1/2',
+    background: 'rgb(245, 247, 253)',
+  },
+
+  logo: {
+    gridColumn: '1/2',
+    whiteSpace: 'nowrap',
+    // 245 247 253; 65 107 208; 222 230 253
   },
 
   contentArea: {
-    gridRow: '2/3',
-    background: '#FDAACC',
+    gridColumn: '2/3',
 
     display: 'grid',
-    gridTemplateColumns: '240px auto',
-  },
-
-  nav: {
-    gridColumn: '1/2',
+    gridTemplateRows: 'min-content auto',
   },
 
   content: {
+    gridRow: '2/3',
+  },
+
+  searchArea: {
+    gridRow: '1/2',
+    display: 'grid',
+    alignItems: 'center',
+    gridTemplateColumns: 'min-content auto',
+    columnGap: '8px',
+    padding: theme.spacing(4),
+  },
+
+  search: {
+    padding: '2px 4px',
+    display: 'flex',
+    alignItems: 'center',
+    background: 'rgb(245, 247, 253)',
+
     gridColumn: '2/3',
   },
-});
+
+  input: {
+    marginLeft: theme.spacing(1),
+    flex: 1,
+  },
+
+  iconButton: {
+    padding: 10,
+  },
+}));
 
 function App() {
   const styles = useStyles();
 
   return (
-    <Container className={styles.app}>
-      <Box className={styles.headerArea} p={1}>
-        <Box>Logo</Box>
-        <Box>Search</Box>
+    <Container maxWidth={false} className={styles.app}>
+      <Box className={styles.navArea}>
+        <Box className={styles.logo}>Anime Day</Box>
+        <Box className={styles.nav}>Nav Content</Box>
       </Box>
       <Box className={styles.contentArea}>
-        <Box className={styles.nav}>Nav</Box>
+        <SearchBar />
         <Box className={styles.content}>Content</Box>
       </Box>
     </Container>
---
+++ src/components/SearchBar.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import { Box, Paper, IconButton, InputBase } from '@material-ui/core';
+import SearchIcon from '@material-ui/icons/Search';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    gridRow: '1/2',
+    display: 'grid',
+    alignItems: 'center',
+    gridTemplateColumns: 'min-content auto',
+    columnGap: '8px',
+    padding: theme.spacing(4),
+  },
+
+  search: {
+    padding: '2px 4px',
+    display: 'flex',
+    alignItems: 'center',
+    background: 'rgb(245, 247, 253)',
+
+    gridColumn: '2/3',
+  },
+
+  input: {
+    marginLeft: theme.spacing(1),
+    flex: 1,
+  },
+
+  iconButton: {
+    padding: 10,
+  },
+}));
+
+const SearchBar = () => {
+  const styles = useStyles();
+
+  return (
+    <Box className={styles.root}>
+      <Paper className={styles.search}>
+        <IconButton type="submit" className={styles.iconButton} aria-label="search">
+          <SearchIcon />
+        </IconButton>
+        <InputBase
+          className={styles.input}
+          placeholder="Filter anime..."
+          inputProps={{ 'aria-label': 'Filter anime' }}
+        />
+      </Paper>
+    </Box>
+  );
+};
+
+export default SearchBar;
